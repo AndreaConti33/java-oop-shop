2af7d5f180838cb3d7c4e054b41fabb29ccb97a7
AndreaConti <andrea.conti23331@gmail.com>
2021-11-25 16:10:26 +0100

    Initial commit

---
+++ src/it/shop/Main.java
@@ -0,0 +1,36 @@
+package it.shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Nuovo prodotto:");
+		Prodotto prodotto1 = new Prodotto();
+		
+		System.out.println("Nome esteso: "+ prodotto1.nomeEsteso() + "\nCodice prodotto: " + prodotto1.getCodice() + "\nNome del proddotto: " + prodotto1.getNome() + "\nDescrizione del prodotto: " + prodotto1.getDescrizione() + "\nPrezzo del prodotto senza IVA: " + prodotto1.getPrezzo() + " euro" + "\nPrezzo del prodottto con IVA: " + prodotto1.stampaPrezzoConIva() + " euro");
+
+//		// Costruttore di debug
+//		System.out.println("Nuovo prodotto (debug): ");
+//
+//		// Debug
+//		Prodotto prodottoDebug = new Prodotto(0, "Uova", "10x10", 5, 0.22);
+//
+//		System.out.println("Nome esteso: " + prodottoDebug.nomeEsteso() + "\nCodice prodotto: "
+//				+ prodottoDebug.getCodice() + "\nNome del proddotto: " + prodottoDebug.getNome()
+//				+ "\nDescrizione del prodotto: " + prodottoDebug.getDescrizione() + "\nPrezzo del prodotto senza IVA: "
+//				+ prodottoDebug.getPrezzo() + " euro" + "\nPrezzo del prodottto con IVA: "
+//				+ prodottoDebug.stampaPrezzoConIva());
+//
+//		prodottoDebug.setNome(null);
+//		prodottoDebug.setDescrizione(null);
+//		prodottoDebug.setPrezzo(0);
+//		prodottoDebug.setIva(0);
+//
+//		System.out.println("Nome esteso: " + prodottoDebug.nomeEsteso() + "\nCodice prodotto: "
+//				+ prodottoDebug.getCodice() + "\nNome del proddotto: " + prodottoDebug.getNome()
+//				+ "\nDescrizione del prodotto: " + prodottoDebug.getDescrizione() + "\nPrezzo del prodotto senza IVA: "
+//				+ prodottoDebug.getPrezzo() + " euro" + "\nPrezzo del prodottto con IVA: "
+//				+ prodottoDebug.stampaPrezzoConIva());
+	}
+
+}
---
+++ src/it/shop/Prodotto.java
@@ -0,0 +1,114 @@
+package it.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+
+	// Attributi
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private int prezzo;
+	private double iva;
+
+	// Costruttore#1
+	public Prodotto() {
+		this.codice = generatoreDiCodici();
+
+	}
+
+	// Costruttore#2
+	public Prodotto(int codice, String nome, String descrizione, int prezzo, double iva) {
+		this.codice = generatoreDiCodici();
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+
+	// Getter and Setter
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (isNullorEmptyString(nome)) {
+			System.out.println("Il campo \"nome\" non può essere vuoto.");
+		} else {
+			this.nome = nome;
+		}
+
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		if (isNullorEmptyString(descrizione)) {
+			System.out.println("Il campo \"descrizione\"non può essere vuoto.");
+		} else {
+			this.descrizione = descrizione;
+		}
+
+	}
+
+	public int getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(int prezzo) {
+		if (prezzoValido(prezzo)) {
+			this.prezzo = prezzo;
+		} else {
+			System.out.println("La voce \"Prezzo\" deve essere maggiore di \"Zero\".");
+		}
+
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+
+	public double getIva() {
+		return iva;
+	}
+
+	public void setIva(int iva) {
+		if (prezzoValido(iva)) {
+			this.iva = iva;
+		} else {
+			System.out.println("La voce \"Iva\" deve essere maggiore di \"Zero\".");
+		}
+
+	}
+
+	// Metodo che genera e ritorna un codice casuale
+	private int generatoreDiCodici() {
+		Random random = new Random();
+		return random.nextInt(500);
+	}
+
+	// Metodo che verifica se una stringa è nulla o vuota
+	private boolean isNullorEmptyString(String string) {
+		return string == null || string.length() == 0;
+	}
+
+	// Metodo per stampare il prezzo con iva
+	public double stampaPrezzoConIva() {
+		double stampaPrezzoConIva = prezzo + prezzo * iva;
+		return stampaPrezzoConIva;
+	}
+
+	// Metodo per avere codice + nome
+	public String nomeEsteso() {
+		String nomeEsteso = nome + " " + codice;
+		return nomeEsteso;
+	}
+
+	// Metodo che verifica se il prezzo è valido
+	private boolean prezzoValido(int prezzo) {
+		return prezzo != 0.0D && prezzo > 0;
+	}
+}
